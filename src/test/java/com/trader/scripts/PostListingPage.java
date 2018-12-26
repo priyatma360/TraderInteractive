4eed4b7cc15cce7761c06fe561008835386162af
priyatma360 <45681401+priyatma360@users.noreply.github.com>
2018-12-26 09:24:15 +0530

    Test dec

--- src/test/java/com/trader/scripts/PostListingPage.java
+++ src/test/java/com/trader/scripts/PostListingPage.java
@@ -6,7 +6,7 @@ import com.trader.utils.DriverTestCase;
 
 public class PostListingPage extends DriverTestCase {
 
-	/**
+	/**Test_001:
 	 * 'Following buttons and icon are displayed at pop up when click on 'Sell For
 	 * Free' link in header at home page. 1. Sell Your Motorcycle 2. Edit Your
 	 * Listing 3. Cross(X) icon
